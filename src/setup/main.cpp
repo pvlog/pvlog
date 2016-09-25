2c9c0693a2bb2361b64e03fb2e4b678eee683b23
Benjamin <benjaminst123@gmail.com>
2016-09-25 20:42:27 +0200

    Fix database setup

--- src/setup/main.cpp
+++ src/setup/main.cpp
@@ -29,27 +29,27 @@ using pvlib::Pvlib;
 
 
 int main(int argc, char **argv) {
-//	std::string filename;
-//
-//	char *home;
-//	home = getenv("HOME");
-//	if (home == NULL) {
-//		PVLOG_EXCEPT("Could not get environment variable \"HOME\"!");
-//	} else {
-//		filename = std::string(home) + "/.pvlog/pvlog.conf";
-//	}
-//
-//	LOG(Info) << "Reading database configuration file.";
-//	ConfigReader configReader(filename);
-//	configReader.parse();
-//
-//	std::string databaseType = configReader.getValue("database_type");
-//	std::string databaseName = configReader.getValue("database_name");
-//	std::string hostname = configReader.getValue("hostname");
-//	std::string port = configReader.getValue("port");
-//	std::string username = configReader.getValue("username");
-//	std::string password = configReader.getValue("password");
-//	LOG(Info) << "Successfully parsed database configuration file.";
+	std::string filename;
+
+	char *home;
+	home = getenv("HOME");
+	if (home == NULL) {
+		PVLOG_EXCEPT("Could not get environment variable \"HOME\"!");
+	} else {
+		filename = std::string(home) + "/.pvlog/pvlog.conf";
+	}
+
+	LOG(Info) << "Reading database configuration file.";
+	ConfigReader configReader(filename);
+	configReader.parse();
+
+	std::string databaseType = configReader.getValue("database_type");
+	std::string databaseName = configReader.getValue("database_name");
+	std::string hostname = configReader.getValue("hostname");
+	std::string port = configReader.getValue("port");
+	std::string username = configReader.getValue("username");
+	std::string password = configReader.getValue("password");
+	LOG(Info) << "Successfully parsed database configuration file.";
 
 	std::string databaseName = "test.db";
 
