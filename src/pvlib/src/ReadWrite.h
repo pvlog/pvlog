f8c32a15e26971de79256589f1d9b438b55f2a73
Benjamin <benjaminst123@gmail.com>
2016-10-17 15:30:29 +0200

    Add missing ReadWrite Interafce

---
+++ src/pvlib/src/ReadWrite.h
@@ -0,0 +1,45 @@
+#ifndef SRC_PVLIB_SRC_READWRITE_H_
+#define SRC_PVLIB_SRC_READWRITE_H_
+
+#include <string>
+
+class ReadWrite {
+public:
+	virtual ~ReadWrite() {}
+
+	/**
+	 * Write data.
+	 *
+	 * @param con connection handle.
+	 * @param data data to write.
+	 * @param len length of data.
+	 *
+	 * @return < 0 if error occurs.
+	 */
+	virtual int write(const uint8_t *data, int len, const std::string &to) = 0;
+
+	int write(const uint8_t *data, int len) {
+		return write(data, len, "");
+	}
+
+	/**
+	 * Read data.
+	 *
+	 * @param con connection handle.
+	 * @param data buffer to read to.
+	 * @param len length of data to read.
+	 * @param timeout timeout in ms
+	 *
+	 * @return < 0 if error occurs, else amount of bytes read.
+	 */
+	virtual int read(uint8_t *data, int maxlen, std::string &from) = 0;
+
+	int read(uint8_t *data, int max_len) {
+		std::string str;
+		return read(data, max_len, str);
+	}
+};
+
+
+
+#endif /* SRC_PVLIB_SRC_READWRITE_H_ */
