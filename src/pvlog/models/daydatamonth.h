3d1779a32b4a7dd780926d48d8f2db8f7de51e02
Benjamin <benjaminst123@gmail.com>
2017-05-16 14:47:37 +0200

    Add copyright header

--- src/pvlog/models/daydatamonth.h
+++ src/pvlog/models/daydatamonth.h
@@ -1,8 +1,20 @@
 /*
- * DayDataMonth.h
+ * This file is part of Pvlog.
  *
- *  Created on: Nov 8, 2016
- *      Author: benjamin
+ * Copyright (C) 2017 pvlogdev@gmail.com
+ *
+ * Pvlog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pvlog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pvlog.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef SRC_PVLOG_MODELS_DAYDATAMONTH_H_
